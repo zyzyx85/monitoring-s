8505a9558dd11f1eba8cd974e94a1b8c8a6aa266
Federico Gallucci <federico.gallucci@spindox.it>
2014-12-04 07:37:57 +0100

    Add header for cars!

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ var app = express();
 var cpuHealth = cpuhealth();
 var ramHealth = ramhealth();
 
-app.use(cors({
-  allowedOrigins: [
-    '192.168.33.171'
-  ]
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 
 app.get('/', function (req, res) {
